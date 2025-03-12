4336b0fa14ab9fd4fc4303e5e143b3ea8e63b6be
VitaliiMalatok <palinamalatokpolish@gmail.com>
2025-03-12 23:28:04 +0100

    add SecondTask

--- src/main/java/com/gmail/vitali/malatok/java/FirstTask.java
+++ src/main/java/com/gmail/vitali/malatok/java/FirstTask.java
@@ -3,7 +3,7 @@ package com.gmail.vitali.malatok.java;
 import com.gmail.vitali.malatok.java.service.Impl.RunTaskServiceImpl;
 import com.gmail.vitali.malatok.java.service.RunTaskService;
 
-public class Main {
+public class FirstTask {
     public static void main(String[] args) {
         RunTaskService runTask = new RunTaskServiceImpl();
         runTask.runTaskService();
---
+++ src/main/java/com/gmail/vitali/malatok/java/SecondTask.java
@@ -0,0 +1,10 @@
+package com.gmail.vitali.malatok.java;
+
+public class SecondTask {
+    public static void main(String[] args) {
+        // Every opening bracket has a corresponding closing bracket [((())()(())]]
+        System.out.println("In this bracket sequence, the second-to-last square bracket should be replaced with a round bracket" +
+                "\n" + "[((())()(())]]    ---------->>>>>    [((())()(()))]" +
+                "\n" + "[((())()(()))] it is Corrected sequence");
+    }
+}
