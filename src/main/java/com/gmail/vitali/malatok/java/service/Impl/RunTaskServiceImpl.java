51e1da861a065bbcb301f42917165c85211ac079
VitaliiMalatok <palinamalatokpolish@gmail.com>
2025-03-12 22:55:39 +0100

    add Task_2

--- src/main/java/com/gmail/vitali/malatok/java/service/Impl/RunTaskServiceImpl.java
+++ src/main/java/com/gmail/vitali/malatok/java/service/Impl/RunTaskServiceImpl.java
@@ -4,17 +4,22 @@ import com.gmail.vitali.malatok.java.service.RunTaskService;
 
 import java.util.Scanner;
 
+import static com.gmail.vitali.malatok.java.model.InputValidationMethods.checkName;
 import static com.gmail.vitali.malatok.java.model.InputValidationMethods.checkNumber;
 
 public class RunTaskServiceImpl implements RunTaskService {
     @Override
     public void runTaskService() {
         Scanner scanner = new Scanner(System.in);
-
         // Task 1: Check if number is greater than 7
         System.out.print("Enter a number: ");
         int number = scanner.nextInt();
         checkNumber(number);
+        // Task 2: Check name
+        System.out.print("Enter a name: ");
+        scanner.nextLine(); // Consume the leftover newline
+        String name = scanner.nextLine();
+        checkName(name);
     }
 }
 
